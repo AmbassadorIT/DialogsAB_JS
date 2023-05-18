27286dd2402d9640a752a04eb397638a408a1c31
Arturs Parfjonovs <arturs.parfjonovs@gmail.com>
2023-05-18 20:02:55 +0300

    9. nodarbiba

---
+++ 09_nodarbiba/js/windows.js
@@ -0,0 +1,12 @@
+//var counter = 1;
+//var showCounter = () => console.log(counter);
+
+let counter = 1;
+var showCounter = () => console.log(counter);
+
+console.log(window.counter);
+window.showCounter();
+
+document.getElementById("demo_bom").innerHTML = counter;
+
+window.al
