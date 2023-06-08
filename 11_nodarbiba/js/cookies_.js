58d9e605399f09ebccce966137148cee9b79cc09
Arturs Parfjonovs <arturs.parfjonovs@gmail.com>
2023-06-08 20:52:53 +0300

    Vanilla project

--- 11_nodarbiba/js/cookies_.js
+++ 11_nodarbiba/js/cookies_.js
@@ -1,18 +1,45 @@
-// max cookie size is 4096 bytes or 4KB
+// max cookie size - 4096 bytes (4 KB) - for nname + value
+/*
+1. Cookie nolasīšana
+1.1. Cookie filtrēšana (atlasīšana) (domain + path; hostname:port\path1\path2 būs redzams iekš hostname:port\path1\path2 un hostname:port\path1\path2\path3 utt., bet nebūs redzams iekš hostname:port\path1)
+2. Cookie uzdošana (unikāls name iekš domain + path)
+3. Cookie mainīšana
+4. Cookie "dzīves laika" uzdošana (ja laiks nav uzdots - Session)
+5. Cookie izmantošana pēc pārlūka aizvēršanas, atvēršana
+-------
+6. Cookie kodēšana
+7. Cookie dzēšana
+*/
 
 var str = document.cookie;
-console.log("cookie vērtība:" + str);
+console.log("cookie vērtība: " + str);
 
-document.cookie = "username=admin";
+display("username_1", "admin_1", 30);
+display("username_2", "admin_2");
 
-display();
+//document.cookie = "username_1=admin_1";
+//document.cookie = "username_2=admin_1";
+//str = document.cookie;
+//console.log("cookie vērtība: "+str);
 
-function display() {
-    var now = new Date();
-    var time = now.getTime();
-    var expireTIme = time + 1000*15;
-    now.setTime(expireTIme);
-    document.cookie = `cookie=ok; expires=` + now.toUTCString()+`;path=/`;
-    console.log(document.cookie);
+//document.cookie = "username=admin_2";
+//str = document.cookie;
+//console.log("cookie vērtība: "+str);
 
+// https://stackoverflow.com/questions/13154552/how-can-i-set-a-cookie-with-expire-time
+
+function display(cookie_name, cookie_value, cookie_expire_time_sec) {
+  var now = new Date();
+  var time = now.getTime();
+  if (cookie_expire_time_sec != undefined) {
+    var expireTime = time + 1000 * cookie_expire_time_sec;
+    now.setTime(expireTime);
+    //document.cookie = cookie_name + "=" + cookie_value + ";expires=" + now.toUTCString() + ";path=/";
+  } else {
+    document.cookie = cookie_name + "=" + cookie_value + ";path=/";
+  }
+  console.log(document.cookie);
 }
+
+
+//MAJASDARBS: izzināt DOM
---
+++ Vanilla_JS_project/script.js
@@ -0,0 +1,153 @@
+const balance = document.getElementById('balance');
+const money_plus = document.getElementById('money-plus');
+const money_minus = document.getElementById('money-minus');
+const list = document.getElementById('list');
+const form = document.getElementById('form');
+const text = document.getElementById('text');
+const amount = document.getElementById('amount');
+const totaltramount = document.getElementById('totaltramount');
+
+// const dummyTransactions = [
+//   { id: 1, text: 'Flower', amount: -20 },
+//   { id: 2, text: 'Salary', amount: 300 },
+//   { id: 3, text: 'Book', amount: -10 },
+//   { id: 4, text: 'Camera', amount: 150 }
+// ];
+
+const localStorageTransactions = JSON.parse(
+  localStorage.getItem('transactions')
+);
+
+let transactions =
+  localStorage.getItem('transactions') !== null ? localStorageTransactions : [];
+
+// Add transaction
+function addTransaction(e) {
+  e.preventDefault();
+
+  if (text.value.trim() === '' || amount.value.trim() === '') {
+    alert('Please add a text and amount');
+  } else {
+    const transaction = {
+      id: generateID(),
+      text: text.value,
+      amount: +amount.value
+    };
+
+    transactions.push(transaction);
+
+    addTransactionDOM(transaction);
+
+    updateValues();
+
+    updateLocalStorage();
+
+    text.value = '';
+    amount.value = '';
+
+    showFlashMessage('GOOD, Transaction added successfully!');
+  }
+}
+
+function showFlashMessage(message) {
+  const flashMessage = document.getElementById('flash-message');
+  flashMessage.textContent = message;
+  flashMessage.style.display = 'block';
+
+  // Hide the flash message after 3 seconds
+  setTimeout(() => {
+    flashMessage.style.display = 'none';
+  }, 3000);
+}
+
+// Generate random ID
+function generateID() {
+  return Math.floor(Math.random() * 100000000);
+}
+
+// Add transactions to DOM list
+function addTransactionDOM(transaction) {
+  // Get sign
+  const sign = transaction.amount < 0 ? '-' : '+';
+
+  const item = document.createElement('li');
+
+  // Add class based on value
+  item.classList.add(transaction.amount < 0 ? 'minus' : 'plus');
+
+  item.innerHTML = `
+    ${transaction.text} <span>${sign}${Math.abs(
+    transaction.amount
+  )}</span> <button class="delete-btn" onclick="removeTransaction(${
+    transaction.id
+  })">x</button>
+  `;
+
+  list.appendChild(item);
+}
+
+// Update the balance, income and expense
+function updateValues() {
+  const amounts = transactions.map(transaction => transaction.amount);
+
+  const total = amounts.reduce((acc, item) => (acc += item), 0).toFixed(2);
+
+  const income = amounts
+    .filter(item => item > 0)
+    .reduce((acc, item) => (acc += item), 0)
+    .toFixed(2);
+
+  const expense = (
+    amounts.filter(item => item < 0).reduce((acc, item) => (acc += item), 0) *
+    -1
+  ).toFixed(2);
+
+  balance.innerText = `$${total}`;
+  money_plus.innerText = `$${income}`;
+  money_minus.innerText = `$${expense}`;
+}
+
+// Remove transaction by ID
+function removeTransaction(id) {
+  transactions = transactions.filter(transaction => transaction.id !== id);
+
+  updateLocalStorage();
+
+  init();
+}
+
+
+// Update local storage transactions
+function updateLocalStorage() {
+  localStorage.setItem('transactions', JSON.stringify(transactions));
+}
+
+// Init app
+function init() {
+  list.innerHTML = '';
+
+  transactions.forEach(addTransactionDOM);
+  updateValues();
+}
+
+init();
+
+
+function countTransactionTotals() {
+  const amounts = transactions.map(transaction => transaction.amount);
+  
+  const totalTransactions = amounts.length;
+
+  const total = amounts.reduce((acc, item) => (acc += item), 0).toFixed(2);
+
+  const average = (total / totalTransactions).toFixed(2);
+
+  document.getElementById("totaltramount").innerHTML = "TOTAL TRANSACTION AMOUNT:" + " " + totalTransactions;
+  document.getElementById("totalsumamount").innerHTML = "TOTAL SUM AMOUNT:" + " " + total;
+  document.getElementById("averageamount").innerHTML = "AVERAGE AMOUNT:" + " " + average;
+ 
+}
+
+countTransactionTotals();
+
+form.addEventListener('submit', addTransaction);
